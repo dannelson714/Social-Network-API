5a2626ee36296939f51ef0125e0dce3e3c3ae9ed
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-06 20:43:34 -0400

    Completed Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            defaut: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return date.toISOString().split("T") [0];
+            }
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+     },
+)
