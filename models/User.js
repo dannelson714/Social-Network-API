7b5ac6a154bfd906997befd32b9f04f85e03376d
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-06 20:06:31 -0400

    Established User model according to acceptance criteria

--- models/User.js
+++ models/User.js
@@ -1,1 +1,50 @@
 const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill in a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User=model('user', userSchema);
+
+module.exports = User;
+
