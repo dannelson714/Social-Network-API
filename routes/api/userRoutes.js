0c5325cb6512404f386841f45dddfd510ea4e1b4
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-08 09:38:49 -0400

    created controllers associated with User routes.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,6 +15,6 @@ router.route('/').get(getUsers).post(createUser);
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-route.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
 
 module.exports = router;
